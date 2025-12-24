1faee48330fe26d8e57720f9abb3690538351149
aRandomCoder130 <diego.pheraf@icloud.com>
2025-12-24 10:26:48 -0600

    Initial files for MakeCode project

---
+++ jacdac.ts
@@ -0,0 +1,8 @@
+// auto-generated, do not edit.
+namespace myModules {
+
+    //% fixedInstance whenUsed block="sevenSegmentDisplay3"
+    export const sevenSegmentDisplay3 = new modules.SevenSegmentDisplayClient("sevenSegmentDisplay3");
+
+}
+    
